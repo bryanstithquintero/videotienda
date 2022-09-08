26dd34b82d556597149597ca51be2967f88f5a5b
Bryan Quintero <bryanquinterog@gmail.com>
2022-09-08 18:04:48 -0500

    Plantilla proyecto spring Boot

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto de Videotienda
+
+Este proyecto es realizado en el ciclo 3 de MisionTict 20222 UTP usando spring boot
+
+_autor_: Bryan Quintero
---
+++ src/test/java/co/edu/utp/misiontic/bryan/videotienda/VideotiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic.bryan.videotienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
